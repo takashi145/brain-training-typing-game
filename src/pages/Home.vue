005b08831dde78c3b558c16749b2e36ab5080846
takashi145 <t.shuhei0203@gmail.com>
2023-03-20 01:40:07 +0900

    update Home.vue

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,6 +1,12 @@
 <template>
-  <div class="bg-gray-200 min-h-screen flex items-center flex-col">
-    <h1 class="text-3xl my-8 font-bold">脳トレタイピング</h1>
+  <div class="max-w-7xl mx-auto bg-gray-200 min-h-screen flex items-center flex-col">
+    <div class="my-6 space-y-3 pb-8 w-full border-b border-gray-400">
+      <h1 class="text-center text-2xl font-bold">脳トレタイピング</h1>
+      <p class="text-gray-800 text-center">
+        表示された文字の<span class="text-lg font-bold text-red-500 underline">色の名前</span>をローマ字で解答してください。
+      </p>
+    </div>
+    
     <div class="max-w-lg mx-auto bg-gray-100 p-6 rounded-lg shadow-lg w-full">
       <div v-if="isStarted">
         正解数:  {{ score }}
@@ -24,14 +30,49 @@
         >
           {{ currentWord.word }}
         </h2>
-        <input v-model="userInput" @keydown.enter="check" class="px-3 py-2 border-gray-300 border rounded-lg mb-4 w-full"> 
       </div>
+
       <div v-if="!isStarted && startCount >= 0" class="text-3xl text-center mb-8">
         {{ startCount }}
+      </div>
+
+      <div v-if="isStarted || startCount >= 0" class="mb-4">
+        <input v-model="userInput" @keydown.enter="check" class="px-3 py-2 border-gray-500 border rounded-lg w-full focus:outline-none focus:ring-2 focus:border-none" placeholder="入力してください">
+        <p class="mx-2 text-sm text-gray-600">入力後Enterキーを押してください。</p>
       </div>
       <div class="text-center">
         <button v-if="!isStarted && startCount < 0" @click="startCountdown" class="px-4 py-2 w-full bg-blue-600 text-white rounded-lg font-bold hover:bg-blue-700">開始</button>
         <button v-if="isStarted" @click="endGame" class="px-4 py-2 w-full bg-red-600 text-white rounded-lg font-bold hover:bg-red-700">終了</button>
+      </div>
+    </div>
+
+    <div class="mt-8 mx-auto flex justify-center flex-col items-center max-w-xl">
+      <h3 class="text-lg mb-3 underline w-full text-start ">答え方: </h3>
+      <div class="flex flex-wrap items-center space-x-2 ">
+        <div class="flex items-center space-x-3 p-1 shadow-lg border border-gray-400 rounded">
+          <div class="bg-red-500 w-5 h-5 mr-3"></div> : 
+          <p class="">aka</p>
+        </div>
+        <div class="flex items-center space-x-3 p-1 shadow-lg border border-gray-400 rounded">
+          <div class="bg-blue-500 w-5 h-5 mr-3"></div> : 
+          <p class="">ao</p>
+        </div>
+        <div class="flex items-center space-x-3 p-1 shadow-lg border border-gray-400 rounded">
+          <div class="bg-green-500 w-5 h-5 mr-3"></div> : 
+          <p class="">midori</p>
+        </div>
+        <div class="flex items-center space-x-3 p-1 shadow-lg border border-gray-400 rounded">
+          <div class="bg-yellow-500 w-5 h-5 mr-3"></div> : 
+          <p class="">ki</p>
+        </div>
+        <div class="flex items-center space-x-3 p-1 shadow-lg border border-gray-400 rounded">
+          <div class="bg-black w-5 h-5 mr-3"></div> : 
+          <p class="">kuro</p>
+        </div>
+        <div class="flex items-center space-x-3 p-1 shadow-lg border border-gray-400 rounded">
+          <div class="bg-white border-black border w-5 h-5 mr-3"></div> : 
+          <p class="">siro</p>
+        </div>  
       </div>
     </div>
   </div>
